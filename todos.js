f3c9cd32f44954df1ee3254a5efb9a4ccbc162b6
mazzaro1 <gustamazzaro@gmail.com>
2020-03-16 14:09:14 -0300

    comando para apagar conteúdo interior da lista
    Função de adicionar novos elementos da lista

--- todos.js
+++ todos.js
@@ -9,6 +9,7 @@ var todos = [
 ];
 
 function renderTodos(){
+    listElement.innerHTML='';
     for(todo of todos){
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
